1f4f47ec1d9429dbf22c4d40b18baad8931d2d13
JoltLaw <joshua.rian04@gmail.com>
2022-11-28 13:59:47 -0700

    init commit

---
+++ src/emails/acount.js
@@ -0,0 +1,29 @@
+const sgMail = require("@sendgrid/mail");
+const { text } = require("express");
+const sendGridAPIKey =
+  "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";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "joshua.rian04@gmail.com",
+    subject: "Welcome to the App",
+    text: `welcome to the app, ${name}. let me know how it works out for you.`,
+  });
+};
+
+const goodbyeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "joshua.rian04@gmail.com",
+    subject: `Goodbye ${name}`,
+    text: `Goodbye ${name}, we are sorry to see you go. Is there anything we could've done to have kept you using our app?`,
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  goodbyeEmail,
+};
---
+++ src/models/user.js
@@ -0,0 +1,124 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
+const Task = require("./task");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 7,
+      trim: true,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error(
+            "Password should not include the word password in it"
+          );
+        }
+      },
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("Age must be a positive number");
+        }
+      },
+    },
+    avatar: {
+      type: Buffer,
+    },
+
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
+});
+
+userSchema.methods.toJSON = function () {
+  user = this;
+  const userObject = this.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  delete userObject.avatar;
+
+  return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+
+  user.tokens = user.tokens.concat({ token });
+
+  await user.save();
+
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to log in");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+
+  return user;
+};
+
+// Hash the plane text password before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+// Delete tasks of a deleted user
+userSchema.pre("remove", async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
